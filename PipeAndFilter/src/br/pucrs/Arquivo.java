ce6b01294f4e1bc5cb6f7ee47f72c43c1d1437cd
15202606 <jean.bainha@acad.pucrs.br>
2019-03-13 20:37:58 -0300

    Fundamentos de construção de software

---
+++ PipeAndFilter/src/br/pucrs/Arquivo.java
@@ -0,0 +1,79 @@
+package br.pucrs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class Arquivo {
+	
+	//Caminho para leitura e escrita dos arquivos
+	private static final String PATH = "C:\\cs\\";
+	
+	public static String lerNomeArq() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Por favor, digite o nome do arquivo-texto de entrada: ");
+		
+		String fileName = input.nextLine();
+		input.close();
+		
+		return fileName;
+	}
+	
+	public static void lerArquivo(String fileName, HashMap<String, ArrayList<Integer>> map) {
+		
+		//Tenta abrir o arquivo 
+		try (BufferedReader reader = new BufferedReader(new FileReader(new File(PATH + fileName)))) {
+			String line;
+			int lineNumber = 0;
+
+			//Le o arquivo, uma linha por vez
+			while (reader.ready()) {
+				lineNumber++;
+				line = reader.readLine();
+
+				//Usa uma REGEX para separar palavras de caracteres especiais
+				String[] sArray = line.split("[^\\w']+");
+				
+				//Salva num HashMap a palavra e a linha em que ocorreu
+				for (String s : sArray) {
+					if (s.isEmpty())
+						continue;
+					if (map.containsKey(s)) {
+						map.get(s).add(lineNumber);
+					} else {
+						ArrayList<Integer> a = new ArrayList<>();
+						a.add(lineNumber);
+						map.put(s, a);
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("ERRO: arquivo inexistente.");
+		}
+	}
+
+		
+	public static void salvarSaida(String fileName, HashMap<String, ArrayList<Integer>> map) {
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(PATH + fileName + "-xref.txt"), "utf-8"))) {
+			
+			//writer.write(map.toString());
+			for(String key : map.keySet()) {
+				writer.write(key + ";" + map.get(key)+"\r");
+			}
+			
+			System.out.println("Análise realizada. Arquivo " + fileName + "-xref.txt gerado");
+		} catch (Exception e) {
+			System.out.println("ERRO: não foi possível criar arquivo.");
+		}
+		
+	}
+	
+}
