c53abd52f7275c04ca884ffe79642edfbcdc2e5a
15202606 <jean.bainha@acad.pucrs.br>
2019-03-13 20:55:26 -0300

    Remoção da dependência de PATH

--- PipeAndFilter/src/br/pucrs/App.java
+++ PipeAndFilter/src/br/pucrs/App.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class App {
-	
+
 	private static String fileName;
 	private static HashMap<String, ArrayList<Integer>> map;
 
 	public static void main(String args[]) {
-		
+
 		map = new HashMap<>();
 		fileName = Arquivo.lerNomeArq();
-		
+
 		Arquivo.lerArquivo(fileName, map);
-		
+
 		Arquivo.salvarSaida(fileName, map);
 
 	}
--- PipeAndFilter/src/br/pucrs/Arquivo.java
+++ PipeAndFilter/src/br/pucrs/Arquivo.java
@@ -11,38 +11,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-
 public class Arquivo {
-	
-	//Caminho para leitura e escrita dos arquivos
-	private static final String PATH = "C:\\cs\\";
-	
+
 	public static String lerNomeArq() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Por favor, digite o nome do arquivo-texto de entrada: ");
-		
+
 		String fileName = input.nextLine();
 		input.close();
-		
+
 		return fileName;
 	}
-	
+
 	public static void lerArquivo(String fileName, HashMap<String, ArrayList<Integer>> map) {
-		
-		//Tenta abrir o arquivo 
-		try (BufferedReader reader = new BufferedReader(new FileReader(new File(PATH + fileName)))) {
+
+		// Tenta abrir o arquivo
+		try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
 			String line;
 			int lineNumber = 0;
 
-			//Le o arquivo, uma linha por vez
+			// Le o arquivo, uma linha por vez
 			while (reader.ready()) {
 				lineNumber++;
 				line = reader.readLine();
 
-				//Usa uma REGEX para separar palavras de caracteres especiais
+				// Usa uma REGEX para separar palavras de caracteres especiais
 				String[] sArray = line.split("[^\\w']+");
-				
-				//Salva num HashMap a palavra e a linha em que ocorreu
+
+				// Salva num HashMap a palavra e a linha em que ocorreu
 				for (String s : sArray) {
 					if (s.isEmpty())
 						continue;
@@ -60,20 +56,20 @@ public class Arquivo {
 		}
 	}
 
-		
 	public static void salvarSaida(String fileName, HashMap<String, ArrayList<Integer>> map) {
-		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(PATH + fileName + "-xref.txt"), "utf-8"))) {
-			
-			//writer.write(map.toString());
-			for(String key : map.keySet()) {
-				writer.write(key + ";" + map.get(key)+"\r");
+		try (Writer writer = new BufferedWriter(
+				new OutputStreamWriter(new FileOutputStream(fileName + "-xref.txt"), "utf-8"))) {
+
+			// writer.write(map.toString());
+			for (String key : map.keySet()) {
+				writer.write(key + ";" + map.get(key) + "\r");
 			}
-			
+
 			System.out.println("Análise realizada. Arquivo " + fileName + "-xref.txt gerado");
 		} catch (Exception e) {
 			System.out.println("ERRO: não foi possível criar arquivo.");
 		}
-		
+
 	}
-	
+
 }
